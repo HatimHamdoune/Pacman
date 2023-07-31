a13da6f842bbbdf08d6fa80155758a54782d7560
crowbar <hatimhamdoune@outlook.com>
2023-07-31 22:34:36 +0100

    Change the way sprites are extracted

--- Sprites.py
+++ Sprites.py
@@ -2,49 +2,35 @@ import pygame
 
 
 class Sprite:
-    def __init__(self, filename) -> None:
-        self._model = pygame.image.load(filename + ".png")
-        self._width = self._model.get_width()
-        self._height = self._model.get_height()
+    def __init__(self, filename, sprite_location, n_sprites, sprite_width, sprite_height) -> None:
+        self.spritesheet = SpriteSheet(filename, sprite_width, sprite_height)
         self.y_coordinate = 0
         self.x_coordinate = 0
-        
-    @property
-    def model(self):
-        return self._model
-    
-    @property
-    def coordinates(self):
-        return self.x_coordinate, self.y_coordinate
-        
-    @property
-    def hitbox(self):
-        return self.x_coordinate, self.x_coordinate + self._width, self.y_coordinate, self.y_coordinate + self._height
-
-    def touches_hitbox(self, second_object):
-        left_edge_contact = self.hitbox[0] <= second_object.hitbox[1] < self.hitbox[1]
-        right_edge_contact = self.hitbox[0] <= second_object.hitbox[0] < self.hitbox[1]
-        top_edge_contact = self.hitbox[2] <= second_object.hitbox[3] < self.hitbox[3]
-        bottom_edge_contact = self.hitbox[2] <= second_object.hitbox[2] < self.hitbox[3]
-        return  (left_edge_contact or right_edge_contact) and (top_edge_contact or bottom_edge_contact)
-
-
-class Pacman(Sprite):
-    def __init__(self, filename) -> None:
-        super().__init__(filename) 
         self.looking_right = True
         self.looking_left = False
         self.looking_up = False
         self.looking_down = False
-        self.invincible = True
-        self.points = 0
-    
-    def respawn(self):
-        self.x_coordinate = 1
-        self.y_coordinate = 1
+        self._sprites = self.spritesheet.get_sprites(sprite_location, n_sprites)
+        self._width = sprite_width
+        self._height = sprite_height
+        self._current_model = 0
+        self._model = self.sprite_collection[self._current_model]
 
-    def change_model(self, filename):
-        self._model = pygame.image.load(filename + ".png")
+    @property
+    def coordinates(self):
+        return self.x_coordinate, self.y_coordinate
+
+    @property
+    def model(self):
+        return self._current_model
+
+    @property
+    def sprite_collection(self):
+        return self._sprites
+
+    @property
+    def hitbox(self):
+        return self.x_coordinate, self.x_coordinate + self._width, self.y_coordinate, self.y_coordinate + self._height
 
     def touches_hitbox(self, second_object):
         left_edge_touches = self.hitbox[0] <= second_object.hitbox[1] < self.hitbox[1]
@@ -53,9 +39,52 @@ class Pacman(Sprite):
         bottom_edge_touches = self.hitbox[2] <= second_object.hitbox[2] < self.hitbox[3]
         return  (left_edge_touches or right_edge_touches) and (top_edge_touches or bottom_edge_touches)
 
+   
+    def next_model(self, desired_model):
+        self._current_model = self.sprites[desired_model]
+
+class Pacman(Sprite):
+    def __init__(self, filename, sprite_location, n_sprites, sprite_width, sprite_height) -> None:
+        super().__init__(filename, sprite_location, n_sprites, sprite_width, sprite_height)
+        self.invincible = True
+        self.points = 0
+        
+    
+    def respawn(self):
+        self.x_coordinate = 1
+        self.y_coordinate = 1
 
 
     
+
+    
 class Ghost(Sprite):
-    def __init__(self, filename) -> None:
-        super().__init__(filename)
+    def __init__(self, filename, sprite_location, n_sprites, sprite_width, sprite_height) -> None:
+        super().__init__(filename, sprite_location, n_sprites, sprite_width, sprite_height)
+
+    def in_proximity(self, second_object):
+        offset = 100
+        left_edge_touches = self.hitbox[0] - offset <= second_object.hitbox[1] < self.hitbox[1] + offset
+        right_edge_touches = self.hitbox[0] - offset <= second_object.hitbox[0] < self.hitbox[1] + offset
+        top_edge_touches = self.hitbox[2] - offset <= second_object.hitbox[3] < self.hitbox[3] + offset
+        bottom_edge_touches = self.hitbox[2] - offset <= second_object.hitbox[2] < self.hitbox[3] + offset 
+        return  (left_edge_touches or right_edge_touches) and (top_edge_touches or bottom_edge_touches)
+
+
+        
+class Fruits(Sprite):
+    def __init__(self, filename, sprite_location, n_sprites, sprite_width, sprite_height) -> None:
+        super().__init__(filename, sprite_location, n_sprites, sprite_width, sprite_height)
+
+class SpriteSheet:
+    def __init__(self, filename, character_width, character_height) -> None:
+        self.spritesheet = pygame.image.load(filename)
+        self.character_width = character_width
+        self.character_height = character_height
+
+    def get_sprites(self, x_coordinate, n_sprites):
+        sprites = []
+        for i in range(n_sprites):
+            sprite = self.spritesheet.subsurface(pygame.Rect(x_coordinate, i * self.character_height, self.character_width, self.character_height))
+            sprites.append(sprite)
+        return sprites
--- main.py
+++ main.py
@@ -4,14 +4,16 @@ import pygame
 class Game:
     WINDOW_SIZE = 600
     def __init__(self) -> None:
-        self.__map = Map()
         self.window_size = Game.WINDOW_SIZE
-        self.window = pygame.display.set_mode(self.window_size, self.window_size)
+        self.window = pygame.display.set_mode((self.window_size, self.window_size))
         pygame.display.set_caption("Pacman")
         self.clock = pygame.time.Clock()
 
-        self.pacman = Sprites.Pacman()
+        pacman_sprite_location, pacman_n_sprites, pacman_width, pacman_height = 848, 12, 51, 49
+        self.pacman = Sprites.Pacman("SpriteSheet.png", pacman_sprite_location, pacman_n_sprites, pacman_width, pacman_height)
         self.ghosts = []
+        fruit_sprite_location, fruit_n_sprites, fruit_width, fruit_height = 596, 7, 53, 49
+        self.fruits = Sprites.Fruits("Spritesheet.png", fruit_sprite_location, fruit_n_sprites, fruit_width, fruit_height)
         self.lives = 3
 
         self.game_still_ongoing = True
@@ -19,3 +21,15 @@ class Game:
         self.level_lost = False
 
         self.main_loop()
+
+    def initialize_ghosts(self):
+        starting_ghost_position = 644
+        number_of_sprites = 8
+        ghost_width = 51
+        ghost_height = 49
+        for i in range(4):
+            self.ghosts.append(Sprites.Ghost("SpriteSheet.png", starting_ghost_position + (ghost_height * i), number_of_sprites, ghost_width, ghost_height))
+
+    
+if __name__ == "__main__":
+    Game()
