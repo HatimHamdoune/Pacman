0d2e17940f5aa91861357ca86f9179402b48f2d3
crowbar <hatimhamdoune@outlook.com>
2023-08-20 21:29:11 +0100

    Correct Pinky spawn position

--- Pinky.py
+++ Pinky.py
@@ -2,7 +2,7 @@ from Characters import Character, Ghost
         
 class Pinky(Ghost):
     SPRITE_LOCATION_X, SPRITE_LOCATION_Y = 1140, 280
-    SPAWN_X = 14
+    SPAWN_X = 16
     SPAWN_Y = 14
     NUMBER_OF_MODELS = 8
     def __init__(self, filename) -> None:
